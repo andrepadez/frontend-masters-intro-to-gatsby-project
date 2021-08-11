2a83deabcb76ee8414f696a95c62fd8589d4ae06
André Padez <andre.padez@gmail.com>
2021-08-11 16:32:54 +0100

    added links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IndexPage () {
   return (
     <main>
       <h1>Gatsby!!!</h1>
+      <Link to='/about'>About page</Link>
     </main>
   );
 }
